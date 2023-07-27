980ecc39be27d52e5e96bdc17b01e045176e248a
Asia <joan.mk7@gmail.com>
2023-07-27 15:49:31 +0200

    Dodaj pliki projektów.

---
+++ bookProjectWeb/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using bookProjectWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace bookProjectWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
